a1a205410ab2ae7d41d05b14dc239add2d402c59
Panasenko Anastasia <104728216+OneRealProblem@users.noreply.github.com>
2025-06-09 14:21:40 +0300

    Delete e_new.rb

--- e_new.rb
+++
@@ -1,7 +0,0 @@
-file_in = open('e/input.txt')
-s1 = file_in.gets.chomp.split("").sort
-flag = 0
-s2 = file_in.gets.chomp.split("").sort
-flag = 1 if s1 == s2
-File.write('e/output.txt',flag)
-file_in.close
